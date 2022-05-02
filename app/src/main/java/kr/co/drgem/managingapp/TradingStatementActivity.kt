8aebac627e2b482618f015d41893083172d63919
joo <yunju891018@gmail.com>
2022-05-02 18:38:20 +0900

    거래명세서요청(empty)

---
+++ app/src/main/java/kr/co/drgem/managingapp/TradingStatementActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.drgem.managingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TradingStatementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trading_statement)
+    }
+}
