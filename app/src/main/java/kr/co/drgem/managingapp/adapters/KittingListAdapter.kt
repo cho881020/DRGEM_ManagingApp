b2a17166d62a09c09a3727979561d525936fe884
joo <yunju891018@gmail.com>
2022-05-04 16:56:04 +0900

    키팅출고-키팅번호요청 화면

---
+++ app/src/main/java/kr/co/drgem/managingapp/adapters/KittingListAdapter.kt
@@ -0,0 +1,17 @@
+package kr.co.drgem.managingapp.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.drgem.managingapp.viewholder.KittingListViewHolder
+
+class KittingListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return KittingListViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = 3
+}
---
+++ app/src/main/java/kr/co/drgem/managingapp/viewholder/KittingListViewHolder.kt
@@ -0,0 +1,16 @@
+package kr.co.drgem.managingapp.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.drgem.managingapp.R
+
+class KittingListViewHolder(parent : ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.kitting_list_item, parent, false)
+) {
+
+    fun bind(){
+
+    }
+
+}
