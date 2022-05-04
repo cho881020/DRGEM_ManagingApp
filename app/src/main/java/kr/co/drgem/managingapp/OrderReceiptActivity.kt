6d0a1ebeb7c6788a8ccc70ddf4afa79ac33b2d93
joo <yunju891018@gmail.com>
2022-05-04 10:12:17 +0900

    발주입고-거래처요청

--- app/src/main/java/kr/co/drgem/managingapp/OrderReceiptActivity.kt
+++ app/src/main/java/kr/co/drgem/managingapp/OrderReceiptActivity.kt
@@ -1,16 +1,30 @@
 package kr.co.drgem.managingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import kr.co.drgem.managingapp.adapters.FindCompanyListAdapter
 import kr.co.drgem.managingapp.databinding.ActivityOrderReceiptBinding
 
-class OrderReceiptActivity : AppCompatActivity() {
+class OrderReceiptActivity : BaseActivity() {
 
     lateinit var binding : ActivityOrderReceiptBinding
+    lateinit var mFindCompanyAdapter : FindCompanyListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_order_receipt)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        mFindCompanyAdapter = FindCompanyListAdapter()
+        binding.findCompanyList.adapter = mFindCompanyAdapter
     }
 }
