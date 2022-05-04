790acf4458d4ff16c7c334305b06cdb266fb55f2
joo <yunju891018@gmail.com>
2022-05-04 14:33:08 +0900

    발주요청-발주번호요청-발주명세요청 화면 수정

--- app/src/main/java/kr/co/drgem/managingapp/adapters/OrderDetailListAdapter.kt
+++ app/src/main/java/kr/co/drgem/managingapp/adapters/OrderDetailListAdapter.kt
@@ -2,11 +2,12 @@ package kr.co.drgem.managingapp.adapters
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kr.co.drgem.managingapp.viewholder.OrderDetailViewHolder
 import kr.co.drgem.managingapp.viewholder.TradingStatementListViewHolder
 
-class TradingStatementListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class OrderDetailListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return TradingStatementListViewHolder(parent)
+        return OrderDetailViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
--- app/src/main/java/kr/co/drgem/managingapp/viewholder/OrderDetailViewHolder.kt
+++ app/src/main/java/kr/co/drgem/managingapp/viewholder/OrderDetailViewHolder.kt
@@ -5,8 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.drgem.managingapp.R
 
-class FindOrderListViewHolder(parent : ViewGroup) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.find_order_list, parent, false)
+class OrderDetailViewHolder(parent : ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.dialog_order_detail_list, parent, false)
 ) {
 
     fun bind(){
