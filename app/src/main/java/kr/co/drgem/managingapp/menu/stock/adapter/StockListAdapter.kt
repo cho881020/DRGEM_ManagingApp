79a47d6cfa06a9d6a4ace73ffbbeebc4c50a2781
joo <yunju891018@gmail.com>
2022-06-03 15:46:11 +0900

    품목정보요청-수정예정

--- app/src/main/java/kr/co/drgem/managingapp/menu/stock/adapter/StockListAdapter.kt
+++ app/src/main/java/kr/co/drgem/managingapp/menu/stock/adapter/StockListAdapter.kt
@@ -3,15 +3,20 @@ package kr.co.drgem.managingapp.menu.stock.adapter
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.drgem.managingapp.menu.stock.viewholder.StockListViewHolder
+import kr.co.drgem.managingapp.models.Pummokdetail
 
-class StockListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class StockListAdapter(
+    val mList : ArrayList<Pummokdetail>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return StockListViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+        when(holder){
+            is StockListViewHolder -> holder.bind(mList[position])
+        }
     }
 
-    override fun getItemCount() = 3
+    override fun getItemCount() = mList.size
 }
--- app/src/main/java/kr/co/drgem/managingapp/menu/stock/viewholder/StockListViewHolder.kt
+++ app/src/main/java/kr/co/drgem/managingapp/menu/stock/viewholder/StockListViewHolder.kt
@@ -2,14 +2,31 @@ package kr.co.drgem.managingapp.menu.stock.viewholder
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.drgem.managingapp.R
+import kr.co.drgem.managingapp.models.Pummokdetail
 
 class StockListViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.stock_list_item, parent, false)
 ) {
+    val pummokcode = itemView.findViewById<TextView>(R.id.pummokcode)
+    val pummyeong = itemView.findViewById<TextView>(R.id.pummyeong)
+    val dobeon_model = itemView.findViewById<TextView>(R.id.dobeon_model)
+    val saying = itemView.findViewById<TextView>(R.id.saying)
+    val danwi = itemView.findViewById<TextView>(R.id.danwi)
+    val location = itemView.findViewById<TextView>(R.id.location)
+    val hyeonjaegosuryang = itemView.findViewById<TextView>(R.id.hyeonjaegosuryang)
 
-    fun bind() {
+    fun bind(data: Pummokdetail) {
+
+        pummokcode.text = data.getPummokcodeHP()
+        pummyeong.text = data.getpummyeongHP()
+        dobeon_model.text = data.getdobeon_modelHP()
+        saying.text = data.getsayingHP()
+        danwi.text = data.getdanwiHP()
+        location.text = data.getlocationHP()
+        hyeonjaegosuryang.text = data.gethyeonjaegosuryangHP()
 
     }
 
